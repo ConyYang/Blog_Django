116ee5eed43158706740dee99a478eed22279115
Yang Yubei <yangyubei0218@gmail.com>
2020-10-09 16:43:39 +0800

    modify admin page, use admin page to track posts

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,30 @@
 from typing import Dict, List
-
+from .models import Post
 from django.shortcuts import render
+
+# dummy data here
 # from django.http import HttpResponse
 
 # Create your views here.
-posts = [
-    {
-        'author': 'Cony Yang',
-        'title': 'A Nice Meal',
-        'Content': 'Beef and tomato noodles',
-        'date_posted': 'October 6, 2020'
-    },
-    {
-        'author': 'Lu Yao',
-        'title': 'A Nice Day',
-        'Content': 'Today is a nice day',
-        'date_posted': 'October 7, 2020'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Cony Yang',
+#         'title': 'A Nice Meal',
+#         'content': 'Beef and tomato noodles',
+#         'date_posted': 'October 6, 2020'
+#     },
+#     {
+#         'author': 'Lu Yao',
+#         'title': 'A Nice Day',
+#         'content': 'Today is a nice day',
+#         'date_posted': 'October 7, 2020'
+#     }
+# ]
 
 
 def home(request):
     context: Dict[str, List[Dict[str, str]]] = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
